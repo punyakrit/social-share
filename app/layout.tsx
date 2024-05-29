ddaaedfa20b2479038aaa22e00a93cc74f0c99aa
Heena <heena.garg@thoughtworks.com>
2024-05-29 16:00:22 +0530

    Updated Footer to Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import FooterApp from "@/components/FooterApp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div>
           <Providers>
             {children}
+            <FooterApp/>
           </Providers>
         </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Page() {
     <div>
       <Appbar/>
       <Home/>
-      <FooterApp/>
     </div>
   );
 }
