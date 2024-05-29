80442b54dc48b8ce93351e50248c1eff06049a47
Heena <heena.garg@thoughtworks.com>
2024-05-29 15:06:04 +0530

    Added the Footer && Added Terms and condition page with Policy Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Appbar from "@/components/Appbar";
 import { Button } from "@/components/ui/button";
 import Home from "@/components/pages/Home";
 import Image from "next/image";
+import FooterApp from "@/components/FooterApp";
 
 export default function Page() {
   return (
     <div>
       <Appbar/>
       <Home/>
+      <FooterApp/>
     </div>
   );
 }
---
+++ app/terms_conditions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function terms_condition() {
+  return (
+    <>
+    <h1 className='text-white text-5xl font-extrabold h-screen flex items-center justify-center'>Terms & Conditions</h1> 
+   </>
+  )
+}
+
+export default terms_condition;
