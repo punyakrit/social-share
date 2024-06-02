6181a0f3ee7a437ff03c72c1c2d4a3d7f3059680
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-06-02 19:57:01 +0530

    public profile view

--- app/(profile)/[profile]/page.tsx
+++ app/(profile)/[profile]/page.tsx
@@ -45,7 +45,7 @@ async function page({ params }: any) {
     <div className="bg-gray-900">
       <div className="flex justify-center h-screen sm:py-3">
         <div
-          className="sm:w-[43%] md:w-1/2 lg:w-1/3 rounded-t-3xl rounded-3xl"
+          className="sm:w-[43%] md:w-1/2 lg:w-1/3 rounded-t-3xl overflow-y-scroll no-scrollbar rounded-3xl"
           style={{ backgroundColor: page.bgColor }}
         >
           <div
@@ -93,6 +93,45 @@ async function page({ params }: any) {
                   </Link>
                 );
               })}
+          </div>
+
+          <div className="max-w-2xl mx-auto space-y-2 gap-6 p-4 px-8 text-white">
+            {/* @ts-ignore */}
+            {page.links.map((l) => (
+              <Link
+                key={l.url}
+                target="_blank"
+                ping={`${process.env.URL}api/click?url=${btoa(l.url)}&page=${
+                  page.uri
+                }`}
+                className="bg-gray-950/90 rounded-3xl p-2 flex items-center"
+                href={l.url}
+              >
+                <div className="relative overflow-hidden w-16">
+                  <div className="w-16 h-16 relative flex items-center justify-center aspect-square">
+                    {l.icon ? (
+                      <Image
+                        className="w-full h-full object-cover bg-gray-950 rounded-full border"
+                        src={l.icon}
+                        alt="icon"
+                        width={64}
+                        height={64}
+                      />
+                    ) : (
+                      <Icons.Link2 className="w-8 h-8" />
+                    )}
+                  </div>
+                </div>
+                <div className="flex flex-grow items-center ml-4">
+                  <div>
+                    <h3>{l.title}</h3>
+                  </div>
+                </div>
+                <div className="flex-shrink-0 ml-auto mr-6">
+                  <Icons.Send />
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
