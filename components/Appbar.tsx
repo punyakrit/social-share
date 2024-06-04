0c236b2b48b481b0400b1ae7f1c6f65e2c0b3a54
Kapil Gupta <Kapilkggupta2@gmail.com>
2024-06-04 13:17:28 +0530

    Header needs to be responsive #74

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -5,12 +5,12 @@ import { getServerSession } from "next-auth";
 import { User } from "lucide-react";
 import { authOptions } from "@/lib/authOptions";
 import Link from "next/link";
-
+import './css/Appbar.css'
 async function Appbar() {
   const session = await getServerSession(authOptions)
   return (
     <div className="z-20 mr-20 h-20 w-full rounded-b-xl bg-black/10 backdrop-blur-sm fixed">
-      <div className=" text-white flex h-full px-6 md:px-20 justify-between items-center ">
+      <div className=" text-white flex h-full px-6 md:px-20 justify-between items-center headerItems">
        <Link href={'/'}>
         <div
           className=" cursor-pointer font-bold  text-3xl"
