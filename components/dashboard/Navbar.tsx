3e4c19d8f1152eb07db51b481e96da74516573bb
Heena <heena.garg@thoughtworks.com>
2024-06-07 10:37:17 +0530

    Added the side bar on small and mobile screen

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -1,22 +1,20 @@
 "use client"
 import { AlignJustify, X } from 'lucide-react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleBar = () => {
         setIsOpen(!isOpen);
+        const styleNav: any = document.getElementsByClassName('sidebar')[0];
+        styleNav.style.display = !isOpen ? 'block' : 'none';
+
     };
 
-    useEffect(() => {
-        const sidebar: any = document.getElementsByClassName('sidebar')[0];
-        if (sidebar) {
-            sidebar.style.display = isOpen ? 'block' : 'none';
-            sidebar.style.color = isOpen ? 'initial' : 'red';
-        }
-    }, [isOpen]);
-
+    
+  
     return (
         <div>
             <button
@@ -24,8 +22,12 @@ function Navbar() {
                 className='absolute left-0 top-0 p-4 lg:hidden text-white'>
                 {!isOpen ? <AlignJustify /> : <X />}
             </button>
+
         </div>
     );
 }
 
 export default Navbar;
+
+
+
