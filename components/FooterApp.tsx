d82fdddfa29b09ed71b9157d9d709062b5234867
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-06-26 12:59:55 +0530

    Update footer.tsx

--- components/FooterApp.tsx
+++ components/FooterApp.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import { Github,  Linkedin } from "lucide-react";
-import XIcon from "./ui/XIcon"
+import { Github, Linkedin } from "lucide-react";
+import XIcon from "./ui/XIcon";
+
 function FooterApp() {
   return (
     <footer className="bg-gray-900 border-t backdrop-blur-sm text-white">
@@ -13,7 +14,10 @@ function FooterApp() {
               </span>
             </Link>
             <p className="text-sm text-gray-300 mt-2 mr-400">
-            ShareHub is a Project where users can <br />create their public viewable profile containing <br />their customizable social links and important links <br />that they want to share with the world. </p>
+              ShareHub is a Project where users can <br />create their public
+              viewable profile containing <br />their customizable social links
+              and important links <br />that they want to share with the world.{" "}
+            </p>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
@@ -21,13 +25,21 @@ function FooterApp() {
                 Follow us
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <Link href="https://github.com/punyakrit/social-share" target="_blank" className="transition ease duration-150 hover:text-white/65 hover:cursor-pointer">
+                <li className="mb-4 hover-scale" >
+                  <Link
+                    href="https://github.com/punyakrit/social-share"
+                    target="_blank"
+                    className="transition ease duration-150 hover:text-white hover:cursor-pointer "
+                  >
                     Github
                   </Link>
                 </li>
-                <li>
-                  <Link href="https://www.linkedin.com/in/punyakrit-singh-makhni/" target="_blank" className="transition ease duration-150 hover:text-white/65 hover:cursor-pointer">
+                <li className="mb-4 hover-scale">
+                  <Link
+                    href="https://www.linkedin.com/in/punyakrit-singh-makhni/"
+                    target="_blank"
+                    className="transition ease duration-150 hover:text-white hover:cursor-pointer hover-scale"
+                  >
                     Linkedin
                   </Link>
                 </li>
@@ -38,18 +50,30 @@ function FooterApp() {
                 Legal
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <Link href="/policy" target="_blank" className="transition ease duration-150 hover:text-white/65 hover:cursor-pointer">
+                <li className="mb-4 hover-scale">
+                  <Link
+                    href="/policy"
+                    target="_blank"
+                    className="transition ease duration-150 hover:text-white hover:cursor-pointer hover-scale"
+                  >
                     Privacy Policy
                   </Link>
                 </li>
-                <li className="mb-4">
-                  <Link href="/terms_conditions" target="_blank" className="transition ease duration-150 hover:text-white/65 hover:cursor-pointer">
+                <li className="mb-4 hover-scale">
+                  <Link
+                    href="/terms_conditions"
+                    target="_blank"
+                    className="transition ease duration-150 hover:text-white hover:cursor-pointer hover-scale"
+                  >
                     Terms &amp; Conditions
                   </Link>
                 </li>
-                <li>
-                  <Link href="/licensing" target="_blank" className="transition ease duration-150 hover:text-white/65 hover:cursor-pointer">
+                <li className="mb-4 hover-scale">
+                  <Link
+                    href="/licensing"
+                    target="_blank"
+                    className="transition ease duration-150 hover:text-white hover:cursor-pointer hover-scale"
+                  >
                     Licensing
                   </Link>
                 </li>
@@ -67,24 +91,23 @@ function FooterApp() {
             . All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            
             <Link
               href="https://www.linkedin.com/in/punyakrit-singh-makhni/"
-              className="text-gray-500 hover:text-white dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-white dark:hover:text-white ms-5 transform-scale"
             >
               <Linkedin className="w-6 h-6" />
               <span className="sr-only">LinkedIn community</span>
             </Link>
             <Link
               href="https://x.com/Punyakrit22"
-              className="text-gray-500 hover:text-white dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-white dark:hover:text-white ms-5 transform-scale"
             >
-             <XIcon className={"fill-gray-500 hover:fill-white w-6"}/> 
+              <XIcon className={"fill-gray-500 hover:fill-white w-6"} />
               <span className="sr-only">Twitter page</span>
             </Link>
             <Link
               href="https://github.com/punyakrit/social-share"
-              className="text-gray-500 hover:text-white dark:hover:text-white ms-5"
+              className="text-gray-500 hover:text-white dark:hover:text-white ms-5 transform-scale"
             >
               <Github className="w-6 h-6" />
               <span className="sr-only">GitHub account</span>
