90131236aab459de1339fce87d099c180199316f
Kapil Gupta <Kapilkggupta2@gmail.com>
2024-07-07 16:31:27 +0530

    [Bug]: home page needs to be responsive #152

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -12,7 +12,7 @@ async function Appbar() {
   const session = await getServerSession(authOptions);
   return (  
     <div className="z-20 mr-20 h-20 w-full mt-0 rounded-b-xl bg-black/10 backdrop-blur-3xl fixed">
-      <div className=" text-white flex h-full md:px-6  px-5 p-1 gap-10 justify-between items-center ">
+      <div className=" text-white flex h-full md:px-6  px-5 p-1 gap-10 justify-between items-center mr-2 ">
         <Link href={"/"}>
           <div className=" cursor-pointer font-bold  text-3xl">ShareHub</div>
         </Link>
