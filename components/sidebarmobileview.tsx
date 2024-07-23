e7e2ebd30e0d4ecfce4bb552d68f5f19bf9b96dd
Sahil Pahuja <19059570004@aryabhattacollege.ac.in>
2024-07-23 21:42:33 +0530

    Added a closing icon to the hamburger menu

--- components/sidebarmobileview.tsx
+++ components/sidebarmobileview.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { HiMenuAlt1 } from "react-icons/hi";
 import Link from "next/link";
+import { HiMenuAlt1 } from "react-icons/hi";
 
 export const SidebarMobileView = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,12 +11,20 @@ export const SidebarMobileView = () => {
       <div className="relative">
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className={`cursor-pointer fa ${isOpen ? 'fa-times' : 'fa-bars'}`}
+          className={`cursor-pointer fa ${isOpen ? "" : "fa-bars"}`}
         >
           {/* <HiMenuAlt1 className="text-3xl transition-colors duration-300" /> */}
         </div>
-        {isOpen && (
-          <div className="fixed inset-0 bg-gray-800 text-white shadow-lg pb-[23.5rem] px-4 pt-2">
+        <div
+          className={`fixed inset-0 bg-gradient-to-t from-[#193054] to-[#091b38] text-white shadow-lg transition-transform duration-300 ease-in-out ${
+            isOpen ? "transform translate-y-0" : "transform -translate-y-full"
+          } pb-[26.5rem] px-4 pt-2`}
+        >
+          <div
+            className="absolute top-5 right-5 cursor-pointer text-xl text-white  fa fa-times"
+            onClick={() => setIsOpen(false)}
+          ></div>
+          <div className="mt-12">
             <div
               className="p-4 hover:text-gray-400 mx-auto border-2 border-slate-500 rounded-md cursor-pointer transition-colors duration-300 text-center"
               onClick={() => setIsOpen(!isOpen)}
@@ -48,7 +56,7 @@ export const SidebarMobileView = () => {
               <Link href="/faq">FAQs</Link>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
